70c987ad0b5c01851aa1ee62e311709a76e31488
Raissa Rodrigues <raissarodrigues.ufjf@gmail.com>
2023-05-30 19:27:41 -0300

    adiciona estado base

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const form = document.forms.entrada;
         form.addEventListener('subimit', envia);
 
+        let estado = 0;
+
         function envia(evento){
             evento.preventDefault();
             console.log('Formulario enviado!');
+            estado++;
+            atualiza();
+        }
+        function atualiza(){
+           const ol = document.querySelector('ol');
+            ol.innerHTML = <li>$(estado)</li>;
         }
